7c29caaa4c1e3acec3630505c632d5937460fd92
iliuagorbunov <iliuagorbunov@gmail.com>
2018-06-06 18:36:00 +0300

    les 16-18 LayoutParams

--- app/src/main/java/com/startproject/startkotlin/MainActivity.kt
+++ app/src/main/java/com/startproject/startkotlin/MainActivity.kt
@@ -4,20 +4,30 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val btnView by lazy { btnActTwo }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        btnView.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this, ActivityTwo::class.java)
-            startActivity(intent)
-        })
+        var linearLayout = LinearLayout(this)
+        var layoutParams = ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+        linearLayout.layoutParams = layoutParams
+
+        var textView = TextView(this)
+        textView.text = "Text"
+
+        var textLayoutParams = ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        textView.layoutParams = textLayoutParams
+        linearLayout.addView(textView)
+
+        setContentView(linearLayout)
     }
 
 }
